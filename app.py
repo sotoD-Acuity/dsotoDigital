c5f88f396f7a7edbe5e38d28a39703e6f540861a
Diego Alberto Soto <polymath-ios@Polymath-macOS.local>
2024-01-15 23:48:04 -0700

    Testing Extra path added to flask app

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index2.html')
 
+@app.route('/about')
+def about():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
